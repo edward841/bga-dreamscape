2c81565d9ffc504e61f8282a2b17e3f62c3643f6
edward841 <edward.niemann841@gmail.com>
2025-02-03 18:59:24 -0600

    This is my tenative database model. Of course I will adjust it as needed.

--- dbmodel.sql
+++ dbmodel.sql
@@ -34,3 +34,28 @@
 -- ALTER TABLE `player` ADD `player_my_custom_field` INT UNSIGNED NOT NULL DEFAULT '0';
 
 ALTER TABLE `player` ADD `custom_order` TINYINT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `action_points` TINYINT UNSIGNED NOT NULL DEFAULT '0';
+ALTER TABLE `player` ADD `used_location` BOOL DEFAULT true;
+ALTER TABLE `player` ADD `sleeper` TINYINT UNSIGNED NOT NULL DEFAULT '1';
+
+CREATE TABLE IF NOT EXISTS `card` (
+  `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `card_type` varchar(16) NOT NULL,
+  `card_type_arg` int(11) NOT NULL,
+  `card_location` varchar(16) NOT NULL,
+  `card_location_arg` int(11) NOT NULL,
+  PRIMARY KEY (`card_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `element` (
+  `element_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `element_type` varchar(8) NOT NULL,
+  `element_color` varchar(10) NOT NULL,
+  `element_zone` varchar(10) NOT NULL,
+  `element_player_id` int(11) NOT NULL,
+  `element_p` TINYINT NOT NULL DEFAULT '0',
+  `element_q` TINYINT NOT NULL DEFAULT '0',
+  `element_r` TINYINT NOT NULL DEFAULT '0',
+  `element_z` TINYINT NOT NULL DEFAULT '0',
+  PRIMARY KEY (`element_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
